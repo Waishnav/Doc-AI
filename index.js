4265a5cc8f9f585ecd8fa6410d7e1046b30f02f7
Priyansh61 <21bec080@iiitdmj.ac.in>
2023-01-22 00:07:46 +0530

    server working finally

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const socketEvent = require("./socket/events")
+
+
 const bodyParser = require('body-parser');
-const socketEvents = require('./socket/events');
 const documentsRoutes = require('./routes/documents');
 
-const PORT = 3000;
-const app = express();
 
-// Connect to MongoDB
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+
+
+const PORT = 3001;
+
+app.use(cors({
+  credentials: true,
+  origin: '*',
+  headers: 'origin'
+}));
+
+
 mongoose.connect("mongodb+srv://mortysmith:X4dThft4RDPbFGf@cluster0.kxrxbee.mongodb.net/?retryWrites=true&w=majority", 
   {
     useNewUrlParser: true,
@@ -18,15 +35,15 @@ mongoose.connect("mongodb+srv://mortysmith:X4dThft4RDPbFGf@cluster0.kxrxbee.mong
 }).then(() => console.log('DB Connected...')).catch((err) => console.error(err));
 
 // Enable CORS
-app.use(cors());
+console.log("hdfds")
 
 app.use(bodyParser.json());
 
 // Use routes
 app.use('/documents', documentsRoutes);
 
-// Initialize socket.io
 const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-socketEvents.init(server);
+  console.log(`Server working on port ${PORT}`);
+})
+
+socketEvent.init();
--- socket/events.js
+++ socket/events.js
@@ -2,65 +2,70 @@ const socketio = require('socket.io');
 const Document = require('../models/document');
 // const Revision = require('../models/revision');
 
-exports.init = (server) => {
-    const io = socketio(server);
-    
-    io.on("connection", socket => {
-        socket.on("get-document", async documentId => {
-          const document = async (documentId) => {
-            if (documentId == null) return
-            const document = await Document.findById(documentId)
-            if (document) return document
-            return await Document.create({ _id: documentId, content: "" })
-          }
-          socket.join(documentId)
-          socket.emit("load-document", document.data)
+exports.init = () => {
+  const io = require("socket.io")(5000, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  })
 
-        socket.on("send-changes", delta => {
-          socket.broadcast.to(documentId).emit("receive-changes", delta)
-        })
+  io.on("connection", socket => {
+    socket.on("get-document", async documentId => {
+      const document = async (documentId) => {
+        if (documentId == null) return
+        const document = await Document.findById(documentId)
+        if (document) return document
+        return await Document.create({ _id: documentId, content: "" })
+      }
+      socket.join(documentId)
+      socket.emit("load-document", document.data)
 
-        socket.on("save-document", async data => {
-          await Document.findByIdAndUpdate(documentId, { content: data })
-        })
+      socket.on("send-changes", delta => {
+        socket.broadcast.to(documentId).emit("receive-changes", delta)
+      })
+
+      socket.on("save-document", async data => {
+        await Document.findByIdAndUpdate(documentId, { content: data })
       })
     })
-    // io.on('connection', (socket) => {
-    //     socket.on('join', async (documentId) => {
-    //         const document = await Document.findById(documentId);
-    //         if (!document) {
-    //             return socket.emit('document:not-found');
-    //         }
-    //         socket.join(documentId);
-    //         socket.emit('document', document.content);
-    //     });
+  })
+  // io.on('connection', (socket) => {
+  //     socket.on('join', async (documentId) => {
+  //         const document = await Document.findById(documentId);
+  //         if (!document) {
+  //             return socket.emit('document:not-found');
+  //         }
+  //         socket.join(documentId);
+  //         socket.emit('document', document.content);
+  //     });
 
-    //     socket.on('change', async (data) => {
-    //         const { documentId, delta } = data;
-    //         const document = await Document.findById(documentId);
-    //         if (!document) {
-    //             return socket.emit('document:not-found');
-    //         }
+  //     socket.on('change', async (data) => {
+  //         const { documentId, delta } = data;
+  //         const document = await Document.findById(documentId);
+  //         if (!document) {
+  //             return socket.emit('document:not-found');
+  //         }
 
-    //         // Save the changes as a new revision
-    //         // const revision = new Revision({
-    //         //     document: document._id,
-    //         //     content: document.content,
-    //         //     delta: delta
-    //         // });
-    //         // await revision.save();
+  //         // Save the changes as a new revision
+  //         // const revision = new Revision({
+  //         //     document: document._id,
+  //         //     content: document.content,
+  //         //     delta: delta
+  //         // });
+  //         // await revision.save();
 
-    //         // Update the document content
-    //         document.content = delta;
-    //         document.updatedAt = Date.now();
-    //         await document.save();
+  //         // Update the document content
+  //         document.content = delta;
+  //         document.updatedAt = Date.now();
+  //         await document.save();
 
-    //         // Emit the changes to all users connected to the document
-    //         io.to(documentId).emit('change', delta);
-    //     });
+  //         // Emit the changes to all users connected to the document
+  //         io.to(documentId).emit('change', delta);
+  //     });
 
-    //     socket.on('disconnect', () => {
-    //         console.log('user disconnected');
-    //     });
-    // });
+  //     socket.on('disconnect', () => {
+  //         console.log('user disconnected');
+  //     });
+  // });
 };
