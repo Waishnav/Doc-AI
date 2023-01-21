1e40f50bee087857a1ab337a3b159374a0fffc9a
tushitshukla03 <tushit.shukla03@gmail.com>
2023-01-21 17:17:15 +0530

    user_schema

---
+++ models/users.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+    }
+})
+
+const User = mongoose.model("User", UserSchema);
+export default User;
