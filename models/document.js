ff4825c3aa59b3274ab7e10fd63753a43a8c8ceb
Waishnav <waishnavdeore@gmail.com>
2023-01-21 10:53:41 +0000

    added documents model

---
+++ models/document.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const documentSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    access: {
+        type: String,
+        enum: ['private', 'public'],
+        default: 'private'
+    },
+    collaborators: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }]
+});
+
+const Document = mongoose.model('Document', documentSchema);
+
+module.exports = Document;
