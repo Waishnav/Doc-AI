b91792b337a43fe7ada4416faea038e070148c97
Priyansh61 <21bec080@iiitdmj.ac.in>
2023-01-22 04:49:28 +0530

    nits

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,8 @@ const promiseHandler= fn => (req, res, next) => {
 router.post('/register',  async (req,res,next) => {
     try {
         const user = await userController.register(req.body);
-        res.status(201).json({ user });
+        console.log("req", req.body,"user",user)
+        res.status(201).json({message: "succefully register", user });
     } catch (error) {
         res.status(500).json({ error });
     }
@@ -19,7 +20,8 @@ router.post('/register',  async (req,res,next) => {
 router.post('/login',  async (req,res,nex) => {
     try {
         const user = await userController.login(req.body);
-        res.status(200).json({ user });
+        console.log("req", req.body,"user",user)
+        res.status(200).json({message: "succefully login", user });
     }
     catch (error) {
         res.status(500).json({ error });
